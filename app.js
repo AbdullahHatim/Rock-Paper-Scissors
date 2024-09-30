9129eef1f006cd296cc96b5be633d88f6b73e173
Abdullah Hatim <182906341+AbdullahHatim@users.noreply.github.com>
2024-09-30 10:27:30 -0700

    feat: add getHumanChoice()
    also moved choices outside functions as a constant

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
-function getComputerChoice(){
-    let choices = ['Rock', 'Paper', 'Scissors'];
-    let randomChoice = Math.floor((Math.random() * 3));
+const choices = ["Rock", "Paper", "Scissors"];
 
-    return choices[randomChoice];
+function getComputerChoice() {
+  let randomChoice = Math.floor(Math.random() * 3);
+
+  return choices[randomChoice];
+}
+
+function getHumanChoice() {
+  let humanChoice = +prompt(
+    "Select an option:\n1. Rock\n2. Paper\n3. Scissors"
+  );
+
+  //Patch me!
+  return choices[humanChoice - 1];
 }
