63a706064251f19ca97b3da8af9e48edca210c68
Abdullah Hatim <182906341+AbdullahHatim@users.noreply.github.com>
2024-09-30 10:47:09 -0700

    fix: update getHumanChoice() to handle wrong input
    before a user was able to input anything and that results in undefined
    now we account for that

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const choices = ["Rock", "Paper", "Scissors"];
+let humanScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
   let randomChoice = Math.floor(Math.random() * 3);
@@ -7,10 +9,12 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-  let humanChoice = +prompt(
-    "Select an option:\n1. Rock\n2. Paper\n3. Scissors"
-  );
+  let humanChoice =
+    +prompt("Select an option:\n1. Rock\n2. Paper\n3. Scissors") - 1;
 
-  //Patch me!
-  return choices[humanChoice - 1];
+  if (humanChoice > 2 || humanChoice < 0 || isNaN(humanChoice)) {
+    alert("I'll choose for you then...");
+    humanChoice = Math.floor(Math.random() * 3);
+  }
+  return choices[humanChoice];
 }
